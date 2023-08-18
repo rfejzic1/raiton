9adb0507b3ab33cc4c5759d4da39e06487ad78f7
Rijad Fejzic <rijadfejzic@gmail.com>
2023-08-19 00:34:02 +0200

    Simple CLI app to read file and print tokens

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/rfejzic1/raiton/lexer"
+)
 
 func main() {
-	fmt.Println("Hello, Raiton!")
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: lang <path-to-file>")
+	}
+
+	data, err := os.ReadFile(os.Args[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	l := lexer.New([]rune(string(data)))
+
+	for token := l.Next(); token.Type != lexer.EOF; token = l.Next() {
+		token.Print()
+	}
 }
