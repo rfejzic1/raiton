25c504e9ed59167261bcc81cae89d062f9bb95c6
Rijad Fejzic <rijadfejzic@gmail.com>
2023-10-13 09:03:31 +0200

    Fresh start

--- cli/cli.go
+++ cli/cli.go
@@ -10,33 +10,10 @@ type Cli struct {
 
 func New() Cli {
 	app := &cli.App{
-		Name:    "raiton",
-		Usage:   "the Raiton language toolchain",
-		Version: VERSION,
-		Authors: []*cli.Author{
-			{
-				Name: "rfejzic1",
-			},
-		},
-		Commands: []*cli.Command{
-			{
-				Name:   "repl",
-				Usage:  "start the REPL",
-				Action: repl,
-			},
-			{
-				Name:  "tokenize",
-				Usage: "tokenize the given file",
-				ArgsUsage: "[file path]",
-				Action:    tokenize,
-			},
-			{
-				Name: "parse",
-				Usage: "parse the given file and check for errors",
-				ArgsUsage: "[file path]",
-				Action: parse,
-			},
-		},
+		Name:     "raiton",
+		Usage:    "the Raiton language toolchain",
+		Version:  VERSION,
+		Commands: []*cli.Command{},
 	}
 
 	return Cli{app: app}
