20adc501ffeb4c6c2c29eab118a334459984723d
Rijad Fejzic <rijadfejzic@gmail.com>
2023-10-15 16:34:45 +0200

    Add evaluation to new repl

--- cli/repl.go
+++ cli/repl.go
@@ -2,6 +2,11 @@ package cli
 
 import (
 	"fmt"
+	"raiton/evaluator"
+	"raiton/lexer"
+	"raiton/object"
+	"raiton/parser"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
@@ -10,7 +15,13 @@ import (
 
 type repl struct {
 	textInput textinput.Model
+	env       *object.Environment
+	err       error
+	result    object.Object
 }
+
+type errorMsg error
+type resultMsg object.Object
 
 func initialModel() *repl {
 	ti := textinput.New()
@@ -21,6 +32,7 @@ func initialModel() *repl {
 
 	return &repl{
 		textInput: ti,
+		env:       object.NewEnvironment(),
 	}
 }
 
@@ -34,9 +46,26 @@ func (m *repl) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.Type {
-		case tea.KeyEnter, tea.KeyCtrlC, tea.KeyEsc:
+		case tea.KeyCtrlC:
 			return m, tea.Quit
+		case tea.KeyEnter:
+			input := strings.TrimSpace(m.textInput.Value())
+			m.textInput.Reset()
+
+			if input == "exit" {
+				return m, tea.Quit
+			}
+
+			return m, m.evaluateSource(input)
 		}
+	case resultMsg:
+		m.result = msg
+		m.err = nil
+		return m, nil
+	case errorMsg:
+		m.result = nil
+		m.err = msg
+		return m, nil
 	}
 
 	m.textInput, cmd = m.textInput.Update(msg)
@@ -44,8 +73,46 @@ func (m *repl) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
+func (r *repl) evaluateSource(input string) tea.Cmd {
+	return func() tea.Msg {
+		lex := lexer.New(input)
+		par := parser.New(&lex)
+
+		node, err := par.Parse()
+
+		if err != nil {
+			return errorMsg(err)
+		}
+
+		eval := evaluator.New(r.env)
+
+		result, err := eval.Evaluate(node)
+
+		if err != nil {
+			return errorMsg(err)
+		}
+
+		return resultMsg(result)
+	}
+}
+
 func (m *repl) View() string {
-	return fmt.Sprintf("%s\n(type 'exit' or ctrl+c to quit)", m.textInput.View())
+	var s strings.Builder
+
+	s.WriteString(m.textInput.View())
+	s.WriteString("\n")
+
+	if m.result != nil {
+		s.WriteString(m.result.Inspect())
+		s.WriteString("\n")
+	} else if m.err != nil {
+		s.WriteString(fmt.Sprintf("error: %s", m.err))
+		s.WriteString("\n")
+	}
+
+	s.WriteString("(type 'exit' or ctrl+c to quit)")
+
+	return fmt.Sprintf(s.String())
 }
 
 func runRepl(ctx *cli.Context) error {
